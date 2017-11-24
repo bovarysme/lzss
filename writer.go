3f52eabf520638e3e17c15d9f6e6689225f1a44e
bovarysme <bovarysme@users.noreply.github.com>
2017-11-25 00:16:36 +0100

    Remove the need for io.WriteSeeker

--- writer.go
+++ writer.go
@@ -7,84 +7,81 @@ import (
 
 var ErrWriterClosed = errors.New("lzss: writer is closed")
 
+// A Writer takes data written to it and writes the compressed form of that
+// data to an underlying io.Writer (see NewWriter).
 type Writer struct {
-	w      io.WriteSeeker
+	w      io.Writer
 	window *Window
 
-	flags  int
-	offset int64
-	match  [2]byte
+	flags int
 
-	init bool
-	err  error
+	// In the worst case scenario, 17 bytes have to be stored before being
+	// flushed to the underlying io.Writer: one byte for the current flags, and
+	// 16 bytes for 8 matches (2 bytes each).
+	buffer [17]byte
+	n      int
+
+	err error
 }
 
-func NewWriter(w io.WriteSeeker) *Writer {
+// NewWriter creates a new Writer.
+// Writes to the returned Writer are compressed and written to w.
+//
+// It is the caller's responsibility to call Close on the WriteCloser when
+// done, as writes may be buffered and not flushed until Close.
+func NewWriter(w io.Writer) *Writer {
 	return &Writer{
 		w:      w,
 		window: NewWindow(),
 
 		flags: 1,
+
+		n: 1,
 	}
 }
 
+// Write writes a compressed form of buffer to the underlying io.Writer. The
+// compressed bytes are not necessarily flushed until the Writer is closed.
 func (w *Writer) Write(buffer []byte) (int, error) {
 	if w.err != nil {
 		return 0, w.err
 	}
 
 	n := 0
-
-	if !w.init {
-		w.init = true
-
-		err := w.initFlags()
-		if err != nil {
-			return n, err
-		}
-
-		n++
-	}
-
 	pointer := 0
 
 	for pointer < len(buffer) {
-		length, offset := w.window.FindMatch(buffer[pointer : pointer+maxMatchLength])
+		end := pointer + maxMatchLength
+		if end >= len(buffer) {
+			end = len(buffer) - 1
+		}
+
+		length, offset := w.window.FindMatch(buffer[pointer:end])
 
 		if length < minMatchLength {
 			length = 1
 
-			_, err := w.w.Write(buffer[pointer : pointer+1])
-			if err != nil {
-				return n, err
-			}
+			w.buffer[w.n] = buffer[pointer]
+			w.n++
 
-			n++
 			w.flags = w.flags<<1 | 1
 		} else {
-			w.match[0] = byte(offset & 0xff)
-			w.match[1] = byte((offset&0xf00)>>4 | (length - minMatchLength))
-			_, err := w.w.Write(w.match[:])
-			if err != nil {
-				return n, err
-			}
+			w.buffer[w.n] = byte(offset & 0xff)
+			w.n++
 
-			n += 2
+			w.buffer[w.n] = byte((offset&0xf00)>>4 | (length - minMatchLength))
+			w.n++
+
 			w.flags = w.flags<<1 | 0
 		}
 
 		if w.flags>>8 == 1 {
-			err := w.writeFlags()
+			m, err := w.flushBuffer()
 			if err != nil {
 				return n, err
 			}
 
-			err = w.writeByte(0)
-			if err != nil {
-				return n, err
-			}
-
-			n++
+			n += m
 		}
 
 		w.window.WriteBytes(buffer[pointer : pointer+length])
@@ -95,16 +92,14 @@ func (w *Writer) Write(buffer []byte) (int, error) {
 	return n, nil
 }
 
+// Close closes the Writer, flushing any unwritten data to the underlying
+// io.Writer. It does not close the underlying io.Writer.
 func (w *Writer) Close() error {
 	if w.err != nil {
 		return w.err
 	}
 
-	for w.flags>>8 != 1 {
-		w.flags = w.flags<<1 | 1
-	}
-
-	err := w.writeFlags()
+	_, err := w.flushBuffer()
 	if err != nil {
 		return err
 	}
@@ -114,58 +109,26 @@ func (w *Writer) Close() error {
 	return nil
 }
 
-func (w *Writer) writeByte(b byte) error {
-	var buffer [1]byte
-	buffer[0] = b
-
-	_, err := w.w.Write(buffer[:])
-
-	return err
-}
-
-func (w *Writer) initFlags() error {
-	offset, err := w.w.Seek(0, io.SeekCurrent)
-	if err != nil {
-		return err
-	}
-	w.offset = offset
-
-	err = w.writeByte(0)
-
-	return err
-}
-
-func (w *Writer) writeFlags() error {
-	var flags byte
-
-	offset, err := w.w.Seek(0, io.SeekCurrent)
-	if err != nil {
-		return err
+// flushBuffer flushes the Writer to its underlying io.Writer.
+func (w *Writer) flushBuffer() (int, error) {
+	for w.flags>>8 != 1 {
+		w.flags = w.flags<<1 | 1
 	}
 
-	_, err = w.w.Seek(w.offset, io.SeekStart)
-	if err != nil {
-		return err
-	}
+	w.buffer[0] = reverse(byte(w.flags & 0xff))
 
-	flags = reverse(byte(w.flags & 0xff))
-
-	err = w.writeByte(flags)
+	n, err := w.w.Write(w.buffer[:w.n])
 	if err != nil {
-		return err
+		return n, err
 	}
-
-	_, err = w.w.Seek(offset, io.SeekStart)
-	if err != nil {
-		return err
-	}
-	w.offset = offset
 
 	w.flags = 1
+	w.n = 1
 
-	return nil
+	return n, nil
 }
 
+// reverse reverses bits in a byte.
 func reverse(b byte) byte {
 	b = b&0x55<<1 | b&0xaa>>1
 	b = b&0x33<<2 | b&0xcc>>2
