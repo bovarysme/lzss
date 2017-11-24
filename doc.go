8bd63d110a956e8ee25ca7500712af6ca65b74c5
bovarysme <bovarysme@users.noreply.github.com>
2017-11-25 00:46:24 +0100

    Write package documentation

---
+++ doc.go
@@ -0,0 +1,19 @@
+/*
+Package lzss implements the Lempel-Ziv-Storer-Szymanski compressed data format,
+described in J. A. Storer, ``Data Compression via Textual Substitution'',
+Journal of the ACM, 29(4) (October 1982), pp 928-951.
+
+This example illustrates how to write compressed data to a buffer:
+
+	buffer := new(bytes.Buffer)
+	writer := lzss.NewWriter(buffer)
+	writer.Write([]byte("hello, hello, world\n"))
+	writer.Close()
+
+and how to read that data back:
+
+	reader := lzss.NewReader(buffer)
+	io.Copy(os.Stdout, reader)
+
+*/
+package lzss
